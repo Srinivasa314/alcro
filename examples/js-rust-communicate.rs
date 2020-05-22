9a4689df868f625285203c0cd32c3cbb6967cfeb
Srinivasa <m.srinivasa1234@gmail.com>
2020-05-22 17:42:49 +0530

    Improved example

--- examples/js-rust-communicate.rs
+++ examples/js-rust-communicate.rs
@@ -1,23 +1,34 @@
 #![windows_subsystem = "windows"]
-use alcro::{Content, JSObject, JSResult, UIBuilder};
+use alcro::{Content, UIBuilder};
 use serde_json::to_value;
-
-fn add_one(args: &[JSObject]) -> JSResult {
-    if args.len() != 1 {
-        Err(to_value("One argument needed").unwrap())
-    } else {
-        match args[0].as_i64() {
-            Some(i) => Ok(to_value(i + 1).unwrap()),
-            None => Err(to_value("Not an integer").unwrap()),
-        }
-    }
-}
 
 fn main() {
     let ui = UIBuilder::new()
         .content(Content::Html(include_str!("js-rust-communicate.html")))
         .run();
-    assert_eq!(ui.eval("1+1").unwrap(), 2); //Rust calling js
-    ui.bind("addOne", add_one).unwrap();
+    
+    //Rust calling JS
+    assert_eq!(
+        ui.eval("document.getElementById('title').innerText")
+            .unwrap(),
+        "JS Rust Communication"
+    );
+    ui.eval("document.getElementById('result').innerText='Type the file name in the input box and click the button the result will be displayed'")
+        .unwrap();
+
+    ui.bind("readFile", |args| {
+        if args.len() == 0 {
+            Err(to_value("File name required").unwrap())
+        } else {
+            match args[0].as_str() {
+                Some(name) => match std::fs::read_to_string(name) {
+                    Ok(result) => Ok(to_value(result).unwrap()),
+                    Err(_) => Err(to_value("File cannot be read").unwrap()),
+                },
+                None => Err(to_value("Argument should be a string").unwrap()),
+            }
+        }
+    })
+    .unwrap();
     ui.wait_finish();
 }
