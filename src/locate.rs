fa41d629f3c919f0ad8004bf5c52557ae934a7cd
Srinivasa314 <62195746+Srinivasa314@users.noreply.github.com>
2020-05-22 13:00:26 +0530

    Fix array size bug

--- src/locate.rs
+++ src/locate.rs
@@ -30,7 +30,7 @@ fn paths() -> &'static [&'static str] {
 }
 
 #[cfg(target_os = "windows")]
-fn paths() -> [String; 6] {
+fn paths() -> [String; 7] {
 	return [
 		var("ProgramFiles(x86)").unwrap()+"Microsoft/Edge/Application/msedge.exe",
 		var("LocalAppData").unwrap() + "/Google/Chrome/Application/chrome.exe",
