585e37085b92bc77e79845558c688834e1973c9d
Srinivasa <m.srinivasa1234@gmail.com>
2020-05-23 11:15:49 +0530

    Showed use of bind function in example

--- src/lib.rs
+++ src/lib.rs
@@ -8,8 +8,25 @@
 //! ```
 //! #![windows_subsystem = "windows"]
 //! use alcro::{UIBuilder, Content};
+//! use serde_json::to_value;
+//! 
 //! let ui = UIBuilder::new().content(Content::Html("<html><body>Close Me!</body></html>")).run();
 //! assert_eq!(ui.eval("document.body.innerText").unwrap(), "Close Me!");
+//!
+//! //Expose rust function to js
+//! ui.bind("product",|args| {
+//!     let mut product = 1;
+//!     for arg in args {
+//!         match arg.as_i64() {
+//!             Some(i) => product*=i,
+//!             None => return Err(to_value("Not number").unwrap())
+//!         }
+//!     }
+//!     Ok(to_value(product).unwrap())
+//! });
+//!
+//! assert_eq!(ui.eval("(async () => await product(1,2,3))();").unwrap(), 6);
+//! assert!(ui.eval("(async () => await product(1,2,'hi'))();").is_err()); 
 //! ui.wait_finish();
 //! ```
 
